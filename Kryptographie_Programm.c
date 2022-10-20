678bb8de3949b4ef3618b1a3649ae4e3b5697488
ducanh-ux <40532289+ducanh-ux@users.noreply.github.com>
2022-10-21 00:24:25 +0200

    Ubung 19.10.22 (Aufgabe 2)
    noch nicht fertig

---
+++ Kryptographie_Programm.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+    int i = 0;
+    char c;
+    int sum;
+
+
+        printf("Text eingeben: ");
+        scanf("%c", &c);
+        sum = i + c;
+        sum++;
+
+
+    printf("Summe: %c", sum);
+    getchar();
+    return(0);
+
+}
